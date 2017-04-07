71c717028a147cdfc475276e4588cae98221bdf2
alanzhang001 <alanzhang001@qq.com>
2017-04-07 20:37:34 +0800

    完善todolist

--- todolist/scripts/source/indexMain.js
+++ todolist/scripts/source/indexMain.js
@@ -8,25 +8,25 @@ import Vue from 'vue';
 import DB from './DB';
 
 let todoList = new Vue({
-    el:'#todoContainer',
-    data:{
+    el: '#todoContainer',
+    data: {
         // model对应新输入的todo
-        todo:'',
+        todo: '',
 
         // 输入框palceholder
-        placeholder:'接下俩做点什么',
+        placeholder: '接下俩做点什么',
 
         // 数据库保存的todo
         todolist: DB.fetch(),
 
         // todo的状态
-        STATUS : {
-            ALL:'all',
-            ACTIVE:'active',
-            COMPLETED:'completed'
+        STATUS: {
+            ALL: 'all',
+            ACTIVE: 'active',
+            COMPLETED: 'completed'
         },
 
-        alldone:false,
+        alldone: false,
 
         // 初始筛选的状态
         filterStaus: 'all',
@@ -37,7 +37,7 @@ let todoList = new Vue({
         /**
          * [addNewTodo 保存新的todo]
          */
-        addNewTodo(){
+        addNewTodo() {
             let content = String(this.todo);
             if (content.length === 0) {
                 return;
@@ -47,7 +47,7 @@ let todoList = new Vue({
                 content: content,
                 status: this.STATUS.ACTIVE,
                 // 这个参数加的有点尴尬，主要是为了给input设置值
-                isCompleted:false
+                isCompleted: false
             };
 
             this.todolist.unshift(doto);
@@ -59,7 +59,7 @@ let todoList = new Vue({
          * @param  {[type]} index [description]
          * @return {[type]}       [description]
          */
-        doneTodo: function(index){
+        doneTodo: function(index) {
             // this.todolist[index].isCompleted = !this.todolist[index].isCompleted;
             this.todolist[index].status = this.todolist[index].isCompleted ? this.STATUS.COMPLETED : this.STATUS.ACTIVE;
         },
@@ -69,15 +69,15 @@ let todoList = new Vue({
          * @param  {[type]} index [description]
          * @return {[type]}       [description]
          */
-        deleteTodo(index){
-            this.todolist.splice(index,1);
+        deleteTodo(index) {
+            this.todolist.splice(index, 1);
         },
 
         /**
          * [delteCompleted 删除全部以完成]
          * @return {[type]} [description]
          */
-        delteCompleted(){
+        delteCompleted() {
             this.todolist = this.todolist.filter(item => {
                 return item.status != this.STATUS.COMPLETED;
             });
@@ -87,7 +87,7 @@ let todoList = new Vue({
          * [editTodo 编辑]
          * @return {[type]} [正在编辑的todo]
          */
-        editTodo(todo){
+        editTodo(todo) {
             this.editedTodo = todo;
             this.backup = todo.content;
         },
@@ -97,14 +97,14 @@ let todoList = new Vue({
          * @param  {[type]} todo [description]
          * @return {[type]}      [description]
          */
-        doneEdit(todo,index){
-            console.log(todo)
-            if(!todo.content) {
+        doneEdit(todo, index) {
+
+            if (!todo.content) {
                 this.deleteTodo(index);
                 return;
             }
 
-            if(!this.editedTodo) {
+            if (!this.editedTodo) {
                 return;
             }
             todo.content = todo.content.trim();
@@ -117,19 +117,19 @@ let todoList = new Vue({
          * @param  {[type]} todo [description]
          * @return {[type]}      [description]
          */
-        cancelEdit(todo){
+        cancelEdit(todo) {
             this.editedTodo = null;
             this.backup = null;
             todo.content = this.backup;
         }
     },
-    computed:{
+    computed: {
         /**
          * [filterList 筛选出出来的列表]
          */
         filterList() {
 
-            if (this.filterStaus === this.STATUS.ALL){
+            if (this.filterStaus === this.STATUS.ALL) {
                 return this.todolist;
             }
 
@@ -143,22 +143,22 @@ let todoList = new Vue({
          * [remains 未完成的个数]
          * @return {[type]} [description]
          */
-        remains(){
+        remains() {
             return this.todolist.length;
         },
 
-        alldoneTitle(){
-            return  this.alldone ? '全标记为未完成' :'全部标记为已完成';
+        alldoneTitle() {
+            return this.alldone ? '全标记为未完成' : '全部标记为已完成';
         }
     },
-    watch:{
+    watch: {
         /**
          * [todolist todolist做watch，发生数据变更则将数据保存,包括数据的增减，某条数据的状态的改变]
          * @type {Object}
          */
         todolist: {
             deep: true,
-            handler: function(){
+            handler: function() {
                 DB.save(this.todolist);
             }
         },
@@ -176,6 +176,15 @@ let todoList = new Vue({
                     item.status = status;
                     item.isCompleted = isCompleted;
                 });
+            }
+        }
+    },
+    directives:{
+        focus:{
+            update: function(el, binding, vnode){
+                if (binding.value) {
+                    el.focus();
+                }
             }
         }
     }
