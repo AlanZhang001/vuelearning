d8125ac3ff3dc1aca9fa62e291a2ac5b5f8b7ad0
alanzhang001 <alanzhang001@qq.com>
2018-09-22 16:29:23 +0800

    更新环境

---
+++ client/scripts/app/test/pages/View.vue
@@ -0,0 +1,13 @@
+<style></style>
+<template>
+<div>
+    {{words}}
+</div>
+</template>
+<script>
+export default {
+    data: function(){
+        words:'hello world'
+    }
+};
+</script>
---
+++ client/scripts/app/test/testMain.js
@@ -0,0 +1,32 @@
+/**
+ * testDemo
+ *
+ */
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+const View = () => import ('./pages/View.vue');
+
+Vue.use(VueRouter);
+
+var router = new VueRouter({
+    mode: 'history',
+    base: '/',
+    routes: [{
+        path: '/',
+        redirect: '/view'
+    },{
+        path: '/view',
+        component: View
+    }]
+});
+
+new Vue({
+    el: '#app',
+    router: router,
+    data: function() {
+        return {
+        };
+    },
+});
