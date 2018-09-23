00548feb5acbf364fe8218635d2306e7b34c09b6
alanzhang001 <alanzhang001@qq.com>
2018-09-23 19:34:17 +0800

    vuex

--- client/scripts/app/test/testMain.js
+++ client/scripts/app/test/testMain.js
@@ -14,16 +14,63 @@ const List = () => import ('./pages/List.vue');
 Vue.use(Vuex);
 Vue.use(VueRouter);
 
+const moduleA = {
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment(state) {
+            // 这里的 `state` 对象是模块的局部状态
+            state.count++
+        }
+    },
+
+    getters: {
+        doubleCount(state) {
+            return state.count * 2
+        },
+        sumWithRootCount(state, getters, rootState) {
+            return state.count + rootState.count
+        }
+    },
+    actions: {
+        incrementIfOddOnRootSum({
+            state,
+            commit,
+            rootState
+        }) {
+            if ((state.count + rootState.count) % 2 === 1) {
+                commit('increment')
+            }
+        }
+    }
+};
+
+const moduleB = {
+    state: {},
+    mutations: {},
+    actions: {}
+};
+
+const store = new Vuex.Store({
+    modules: {
+        a: moduleA,
+        b: moduleB
+    }
+});
+
+
 var router = new VueRouter({
     mode: 'history',
     base: '/test',
+    store: store,
     routes: [{
         path: '/',
         redirect: '/list'
-    },{
+    }, {
         path: '/view',
         component: View
-    },,{
+    }, , {
         path: '/list',
         component: List
     }]
