4d5f52c2957b12c64cbf54a62fb8faedccfe33a7
alanzhang001 <alanzhang001@qq.com>
2018-10-27 20:52:51 +0800

    sql

---
+++ server/database/SQL编辑器.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO `movie_search` (`id`, `name`, `link`, `dl`, `createdAt`, `updatedAt`, `size`, `count`, `record_date`)
+VALUES
+	(1,'碟中谍5-碟中谍5下载-Mission Impossible 5.rmvb','https://cnbtspread.xyz/38b798f4dc109a37288d363bcc513148e6206849/','magnet:?xt=urn:btih:38b798f4dc109a37288d363bcc513148e6206849&dn=%E7%A2%9F%E4%B8%AD%E8%B0%8D5-%E7%A2%9F%E4%B8%AD%E8%B0%8D5%E4%B8%8B%E8%BD%BD-Mission%20Impossible%205.rmvb','2018-10-14 09:34:20','2018-10-14 09:34:20','1.16 GB',1,'2015-08-20 05:49:42'),
+	(2,'速度与激情7部曲','https://cnbtspread.xyz/eee4e8f850ae8175b73d7d9fd74acc5bb3ca0549/','magnet:?xt=urn:btih:eee4e8f850ae8175b73d7d9fd74acc5bb3ca0549&dn=%E9%80%9F%E5%BA%A6%E4%B8%8E%E6%BF%80%E6%83%857%E9%83%A8%E6%9B%B2','2018-10-14 09:34:20','2018-10-14 09:34:20','10.48 GB',7,'2015-12-14 03:52:07'),
+	(3,'【性吧论坛.com】PPPD697New ','//cnbtspread.xyz/b5cb2221dd314e6042e958b5a4905087c89b5910/','magnet:?xt=urn:btih:b5cb2221dd314e6042e958b5a4905087c89b5910&dn=%E3%80%90%E6%80%A7%E5%90%A7%E8%AE%BA%E5%9D%9B.com%E3%80%91PPPD697','2018-10-14 09:47:23','2018-10-14 09:47:23','5.28 GB',23,NULL);
+
+ 
+UNLOCK TABLES;
