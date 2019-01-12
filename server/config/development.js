46e2466a038b8cda76a0818af1f48ab6fcbbe83d
alanzhang001 <alanzhang001@qq.com>
2019-01-12 21:52:50 +0800

    config

--- server/config/development.js
+++ server/config/development.js
@@ -30,12 +30,12 @@ let config = {
         ],
         // 站点的爬虫设置
         spread: {
-            domain: 'http://cnbtspread.co',
+            domain: 'http://cnbtspread.me/',
             // ua: 'Mozilla/5.0 (Linux;u;Android 4.2.2;zh-cn;) AppleWebKit/534.46 (KHTML,likeGecko) Version/5.1 Mobile Safari/10600.6.3 (compatible; Baiduspider/2.0;+http://www.baidu.com/search/spider.html)',
             // ua: 'Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)',
             // ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
             userAgent:'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
-            referer: 'https://cnbtspread.xyz/',
+            referer: 'http://cnbtspread.me/',
             headers: {
                 Cookie: '__cfduid=d9ab8475147a1048ba0fe4384c4e273461539098359; cf_clearance=63728cd4e4a00bb4fe492f4564a7a0b7b5a98fce-1539525258-1800-250'
             },
